99490f1b2f544781b593bc98ed0c552aff4b64e3
Sabrinavf <contactsabrinafernandes@gmail.com>
2023-11-17 20:54:22 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estudo
+{
+    class Exercicio3 { 
+    
+        static void Main(string[] args)
+        {
+            double media;
+            string nome1, nome2;
+            string[] vet;
+            int idade1, idade2;
+
+            vet = Console.ReadLine().Split(' '); 
+            nome1= vet[0];
+            idade1= int.Parse(vet[1]);
+
+            vet = Console.ReadLine().Split(' ');
+            nome2= vet[0];
+            idade2 = int.Parse (vet[1]);
+
+
+            media = (double) (idade1 + idade2) / 2.0;
+
+            Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " + media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
